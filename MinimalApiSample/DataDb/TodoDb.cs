8fc2f9ce7eb79824eaf35886b89a5b082fb62169
ronald1996perdomo <ronald1996perdomo@gmail.com>
2022-12-28 08:24:53 -0400

    Chang mme Name

---
+++ MinimalApiSample/DataDb/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiSample.Models;
+
+namespace MinimalApiSample.DataDb
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+        public DbSet<AllItems> Todos => Set<AllItems>();
+    }
+}
